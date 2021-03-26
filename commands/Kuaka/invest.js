0cac9007d444b7364842bbac76fac6c3e048da74
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-03-26 18:32:44 -0400

    emote test 1

--- commands/Kuaka/invest.js
+++ commands/Kuaka/invest.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
 let membersInvested = player.membersInvested
-global.player = {membersInvested}
+const kuakaEmote = "<:kuakacoin:763799806337613834>"
+global.player = {
+    membersInvested,
+    kuakaEmote
+}
 module.exports = {
     name: 'invest',
     execute(message, args) {
-        message.channel.send('Thanks for investing in Kuakacoin! You are officially swag!')
+        message.channel.send(`Thanks for investing in ${kuakaEmote}! You are officially swag!`)
         membersInvested += 1;
         global.player = {membersInvested}
     },
