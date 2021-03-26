20cd76c9fd8efb6b1ae6593bd22e288358ec8a3a
flamemasternxf1 <flamemasternxf@gmail.com>
2021-03-26 22:28:06 +0000

    something something

--- commands/Misc/changelog.js
+++ commands/Misc/changelog.js
@@ -4,8 +4,8 @@ module.exports = {
     execute(message, args) {
         let changelogEmbed = new Discord.MessageEmbed()
             .setColor('#919191')
-            .setTitle('Major Update 0.14')
-            .setDescription(`--MAJOR UPDATE--\n\n-THE BOT HAS HOSTING NOW! It will be on 24/7 for the foreseeable future!\n\n-Added Saving with help from unpinga! You can Invest as much as you want and never lose your data!\n\n-Added ;invest, which allows you to invest into Kuakacoin, boosting its value!`)
+            .setTitle('Update 0.15')
+            .setDescription(`Prefix Update!\n\n-Changed the Prefix from ; to . \n-Added .help! Use it to learn about the bot's other commands.`)
             .setTimestamp()
             .setFooter('Thanks for reading this!', 'https://cdn.discordapp.com/emojis/551903543515152437.png?v=1');
         message.channel.send(changelogEmbed)
--- commands/Misc/ping.js
+++ commands/Misc/ping.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js");
 module.exports = {
 	name: 'ping',
 	execute(message, args) {
-		message.channel.send('#renamethisto.test')
+		message.channel.send('do you feel accomplished? are you proud of yourself for finding this? clap clap clap.')
 	},
 };
