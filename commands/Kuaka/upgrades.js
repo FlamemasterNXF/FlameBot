64e6ae29c2f2a02ede184b5fc231eb0ed42efeb9
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-03-27 19:48:37 -0400

    am dum

--- commands/Kuaka/upgrades.js
+++ commands/Kuaka/upgrades.js
@@ -6,7 +6,7 @@ module.exports = {
         let kuakaEmbed = new Discord.MessageEmbed()
             .setColor('#bd11ff')
             .setTitle(`${kuakaEmote} Upgrades`)
-            .setDescription(`**this is a upgrade**\nCosts 69 ${kuakaEmote}\n*does something!*\n\n**the real**\nCosts 2 ${kuakaEmote}\n*baanan*\n\n**photo**\nCosts ok ${kuakaEmote}\n*graph*\n\n`)
+            .setDescription(`**this is a upgrade**\nCosts 69 USD\n*does something!*\n\n**the real**\nCosts 2 USD\n*baanan*\n\n**photo**\nCosts ok USD\n*graph*\n\n`)
             .setTimestamp()
             .setFooter(`Make sure to invest in Kuakacoin using .invest!`, 'https://cdn.discordapp.com/emojis/763799806337613834.png?v=1');
         message.channel.send(kuakaEmbed)
