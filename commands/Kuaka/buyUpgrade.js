baf6255a8518fb3c932b69b45a7cf406b31ca5e2
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-03-27 20:05:45 -0400

    upgrades

---
+++ commands/Kuaka/buyUpgrade.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+module.exports = {
+    name: 'buyUpgrade',
+    execute(message, args) {
+        if (!args.length) {
+            return message.channel.send(`Please Provide a Upgrade to buy!`);
+        }
+        let upgradeNames = []
+        upgradeNames[0] = "Kuakacoin Maximum"
+        upgradeNames[1] = "Kuakacoin Minimum"
+
+        if(!args.contains(upgradeNames[0]|upgradeNames[1])){
+            return message.channel.send(`That is not a valid Upgrade!`)
+        }
+        else
+            message.channel.send(`if upgrades were finished this would buy one`)
+    },
+};
--- commands/Kuaka/upgradesMenu.js
+++ commands/Kuaka/upgradesMenu.js
@@ -5,8 +5,8 @@ module.exports = {
     execute(message, args) {
         let kuakaEmbed = new Discord.MessageEmbed()
             .setColor('#bd11ff')
-            .setTitle(`${kuakaEmote} Upgrades`)
-            .setDescription(`**this is a upgrade**\nCosts 69 USD\n*does something!*\n\n**the real**\nCosts 2 USD\n*baanan*\n\n**photo**\nCosts ok USD\n*graph*\n\n`)
+            .setTitle(`${kuakaEmote} Upgrades **WIP**`)
+            .setDescription(`**Kuakacoin Maximum**\nCosts 69 USD\n*Increases the Maximum Value of ${kuakaEmote}*\n\n**Kuakacoin Minimum**\nCosts 2 USD\n*Increases the Minimum Value of ${kuakaEmote}*\n\n**photo**\nCosts ok USD\n*graph*\n\n`)
             .setTimestamp()
             .setFooter(`Make sure to invest in Kuakacoin using .invest!`, 'https://cdn.discordapp.com/emojis/763799806337613834.png?v=1');
         message.channel.send(kuakaEmbed)
