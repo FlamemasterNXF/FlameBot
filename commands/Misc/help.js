304c2e1ed0592b80ad0d6e037f488f11832410d9
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-06-25 23:14:11 -0400

    t

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -6,7 +6,7 @@ module.exports = {
         let helpEmbed = new Discord.MessageEmbed()
             .setColor('#00bf49')
             .setTitle('Command List')
-            .setDescription(`**=Moderation=**\n\n-Kick: Kicks a member from your server, duh.\n\n-Ban: The best command.\n\n-Warn: Warns a member.\n\n**=Misc=**\n\n-Update: Brings up the Changelog from the latest Update.\n\n-Server Info: Shows some basic infromation about your server. [VERY OLD AND OUTDATED, USE NOT RECOMMENDED]\n\n**=Kuakacoin Economy=**\n\n-Value: View the current value of ${kuakaEmote}.\n\n-Invest: Invest in ${kuakaEmote}!`)
+            .setDescription(`**=Moderation=**\n\n-Kick: Kicks a member from your server, duh.\n\n-Ban: The best command.\n\n-Warn: Warns a member.\n\n**=Misc=**\n\n-Update: Brings up the Changelog from the latest Update.\n\n-Server Info: Shows some basic infromation about your server. [VERY OLD AND OUTDATED, USE NOT RECOMMENDED]`)
             .setTimestamp()
             .setFooter('Try using the .ping secret', 'https://cdn.discordapp.com/emojis/701943264743391355.png?v=1');
         message.channel.send(helpEmbed)
