b276e1310e78e22ab2171ca1f7086695ad67540a
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-06-25 23:05:09 -0400

    moar

--- commands/Counting/predict.js
+++ commands/Counting/predict.js
@@ -2,7 +2,7 @@
 //(18,889,465,931,478,580,854,784×3×(refactor mult)×5×
 // (1.25^upgrades bought)×1e38×1e10×(30^10)×((upgrades bought/2)^20)×(1e100^compacts))^(2^squarers)^1.1
 const Discord = require("discord.js");
-const B_E = require('../../break_eternity')
+const B_E = require('break_eternity.js')
 module.exports = {
     name: 'predict',
     execute(message, args) {
