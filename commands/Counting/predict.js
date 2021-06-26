a81f00b9ca612b620c4ac1590e0af30b6c5f8285
FlamemasterNXF <FlamemasterNXF@gmail.com>
2021-06-25 23:40:50 -0400

    g

--- commands/Counting/predict.js
+++ commands/Counting/predict.js
@@ -3,11 +3,12 @@
 // (1.25^upgrades bought)×1e38×1e10×(30^10)×((upgrades bought/2)^20)×(1e100^compacts))^(2^squarers)^1.1
 const Discord = require("discord.js");
 const Decimal = require('break_eternity.js')
+const format = require('Formatting.js')
 module.exports = {
     name: 'predict',
     execute(message, args) {
         let nextNum = new Decimal(18889465931478580854784).times(3).times(1e42).times(5).times(Decimal.pow(1.25, 48)).times(1e38).times(1e10).times(30).pow(10).times(Decimal.pow(24, 20)).times(Decimal.pow(1e100, 1)).pow(2).pow(3).pow(1.1)
-        message.channel.send(`The next number is ${nextNum}`)
+        message.channel.send(`The next number is ${format(nextNum)}`)
         nextNum = nextNum.times(2)
     },
 };
